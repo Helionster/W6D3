6a9f8a5aad8648cad599edc2a96a35979feab610
Jason <jasonzhang501@gmail.com>
2023-09-27 14:01:37 -0400

    Starting P1

---
+++ Routes Project/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  # resources :users
+
+  get '/users', to: 'users#index', as: 'users'
+  post '/users', to: 'users#create'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  get '/users/editx'
+end
