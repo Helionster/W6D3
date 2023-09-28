43e57decc574eb53080534c465ee3b4bb3e17a8b
Jason <jasonzhang501@gmail.com>
2023-09-28 15:20:23 -0400

    Project 2 P3

---
+++ Routes Project/app/controllers/artworks_share_controller.rb
@@ -0,0 +1,3 @@
+class ArtworksShareController < ApplicationController
+    
+end
--- Routes Project/config/routes.rb
+++ Routes Project/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :users
+  resources :users, only: [:create,:destroy,:index,:show,:update]
 
   # get '/users', to: 'users#index', as: 'users'
   # post '/users', to: 'users#create'
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
   # get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
   # get '/users/:id', to: 'users#show', as: 'user'
   # delete '/users/:id', to: 'users#destroy'
+
+  resources :artworks, only: [:create,:destroy,:index,:show,:update]
 end
