de78f8e4c9a6eeec52e7d566a6d072a66d72f947
Jason <jasonzhang501@gmail.com>
2023-09-28 14:48:43 -0400

    from Jason's computer but all Jacob's work

--- Routes Project/app/models/artwork.rb
+++ Routes Project/app/models/artwork.rb
@@ -8,20 +8,22 @@
 #  artist_id :bigint           not null
 #
 class Artwork < ApplicationRecord
-    validates :title, null: false
-    validates :image_url, null: false
-    validates :artist_id, null: false
+    validates :title, presence: true
+    validates :image_url, presence: true
+    validates :artist_id, presence: true
     validates :artist_id, uniqueness: { scope: :title}
 
     belongs_to :artist,
         primary_key: :id,
         foreign_key: :artist_id,
-        class_name: :Artist
+        class_name: :User
     
     has_many :artwork_shares,
         primary_key: :id,
         foreign_key: :artwork_id,
         class_name: :ArtworkShare
 
-    
+    has_many :shared_viewers,
+        through: :artwork_shares,
+        source: :viewer
 end
