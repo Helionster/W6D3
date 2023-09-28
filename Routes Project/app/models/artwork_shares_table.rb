2ad749244552bfe1d57d078cab770390036b7aae
JacobSeidner <jseidne1@binghamton.edu>
2023-09-28 12:17:57 -0400

    added belongs to associations

--- Routes Project/app/models/artwork_shares_table.rb
+++ Routes Project/app/models/artwork_shares_table.rb
@@ -1,4 +1,14 @@
 class ArtworkSharesTable < ApplicationRecord 
     validates :artwork_id, presence: true, uniqueness: true
     validates :viewer_id, presence: true
+
+    belongs_to :viewer,
+    primary_key: :id,
+    foreign_key: :viewer_id,
+    class_name: :User
+
+    belongs_to :artwork,
+    primary_key: :id,
+    foreign_key: :artwork_id,
+    class_name: :ArtworkTable
 end
--- Routes Project/app/models/artwork_table.rb
+++ Routes Project/app/models/artwork_table.rb
@@ -3,4 +3,9 @@ class ArtworkTable < ApplicationRecord
     validates :image_url, null: false
     validates :artist_id, null: false
     validates :artist_id, uniqueness: { scope: :title}
+
+    belongs_to :artist
+    primary_key: :id
+    foreign_key: :artist_id
+    class_name: :User
 end
