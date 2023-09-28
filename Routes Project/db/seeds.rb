a6d35eedbef24d99b8e73e628217b09965eaf4dd
JacobSeidner <jseidne1@binghamton.edu>
2023-09-28 16:07:37 -0400

    resetting primary keys for seeds

--- Routes Project/db/seeds.rb
+++ Routes Project/db/seeds.rb
@@ -6,5 +6,15 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-users = User.create([{username: 'jacob'}, {username: 'jason'}])
-artworks = Artwork.create([{title: 'Mona Lisa', image_url:'ewfrqwer', artist_id: 2}])
+puts 'Destroying tables...'
+    ArtworkShare.destroy_all
+    Artwork.destroy_all
+    User.destroy_all
+
+  puts 'Resetting id sequences...'
+  %w(users artworks artwork_shares).each do |table_name|
+    ApplicationRecord.connection.reset_pk_sequence!(table_name)
+  end
+user1 = User.create({username: 'jacob'})
+user2 = User.create({username: 'jason'})
+artworks = Artwork.create([{title: 'Mona Lisa', image_url:'ewfrqwer', artist_id: user1.id}, {title: 'Medicore stick figure', image_url:'still Jacob working. Fun self depricating humor', artist_id: user2.id}])
